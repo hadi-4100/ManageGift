98338392bbe9b04d0eb37e0fa86d97b6e5327245
Hadi-Koubeissi <hadikoubeissi40@gmail.com>
2026-01-06 15:07:21 +0200

    refactor: enhance resume command with improved permission checks and error handling

--- commands/Giveaways/resume.js
+++ commands/Giveaways/resume.js
@@ -1,77 +1,135 @@
-const { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, PermissionsBitField, MessageFlags } = require("discord.js"),
-    ms = require("ms");
+const {
+    EmbedBuilder,
+    ActionRowBuilder,
+    StringSelectMenuBuilder,
+    PermissionsBitField,
+    MessageFlags,
+  } = require("discord.js"),
+  ms = require("ms");
 
 module.exports = {
-    name: 'resume',
-    description: 'resume a giveaway',
-    group: __dirname,
-    owner: false,
-    premium: false,
-    run: async (client, interaction, guildData, lang) => {
+  name: "resume",
+  description: "resume a giveaway",
+  group: __dirname,
+  owner: false,
+  premium: false,
+  run: async (client, interaction, guildData, lang) => {
+    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
-		// If the member doesn't have enough permissions
-		if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages) && (guildData.plugins.role.enabled && !interaction.member.roles.cache.get(guildData.plugins.role.role))) {
-			return interaction.reply({ content: lang.create.perms, flags: MessageFlags.Ephemeral });
-		}
+    // If the member doesn't have enough permissions
+    if (
+      !interaction.member.permissions.has(
+        PermissionsBitField.Flags.ManageMessages
+      ) &&
+      guildData.plugins.role.enabled &&
+      !interaction.member.roles.cache.get(guildData.plugins.role.role)
+    ) {
+      return interaction.editReply({ content: lang.create.perms });
+    }
 
-        let options = [];
-        const activeegivs = client.manager.giveaways.filter((g) => g.guildId === interaction.guild.id && g.pauseOptions.isPaused === true && !g.isDrop);
+    let options = [];
+    const activeegivs = client.manager.giveaways
+      .filter(
+        (g) =>
+          g.guildId === interaction.guild.id &&
+          g.pauseOptions.isPaused === true &&
+          !g.isDrop
+      )
+      .reverse()
+      .slice(0, 24);
 
-        for (let i = 0; i < activeegivs.length; i++) {
-            let value = activeegivs[i];
-            options.push({
-                label: lang.delete.option1(value),
-                description: lang.resume.autodes + `${ms(value.pauseOptions.unPauseAfter)}`,
-                value: `${value.messageId}`,
-                emoji: `<:botlogo:1024760383677927484>`
-            })
+    if (activeegivs.length === 0) {
+      return interaction.editReply({
+        content: "No paused giveaways found to resume.",
+      });
+    }
+
+    for (let i = 0; i < activeegivs.length; i++) {
+      let value = activeegivs[i];
+      options.push({
+        label: lang.delete.option1(value),
+        description:
+          lang.resume.autodes + `${ms(value.pauseOptions.unPauseAfter || 0)}`,
+        value: `${value.messageId}`,
+        emoji: `<:botlogo:1024760383677927484>`,
+      });
+    }
+
+    options.push({
+      label: lang.cancel.option1,
+      description: lang.cancel.option2,
+      value: `cancel`,
+      emoji: `<:backk:1021855656879341659>`,
+    });
+
+    const resumeMenu = new ActionRowBuilder().addComponents(
+      new StringSelectMenuBuilder()
+        .setCustomId("resume-giveaway")
+        .setPlaceholder(lang.selectmenu.choose)
+        .addOptions(options)
+    );
+
+    const response = await interaction.editReply({
+      content: lang.resume.forresume,
+      components: [resumeMenu],
+    });
+
+    const filter = (i) => interaction.user.id === i.user.id;
+
+    const collector = response.createMessageComponentCollector({
+      filter,
+      max: 1,
+      time: 300000,
+    });
+
+    collector.on("collect", (i) => {
+      if (i.values[0] === "cancel") {
+        i.update({ content: lang.cancel.cancelled, components: [] });
+      } else {
+        const giveaway = client.manager.giveaways.find(
+          (g) => g.messageId === i.values[0]
+        );
+        if (!giveaway) {
+          return i.update({ content: "Giveaway not found.", components: [] });
         }
 
-        options.push({
-            label: lang.cancel.option1,
-            description: lang.cancel.option2,
-            value: `cancel`,
-            emoji: `<:backk:1021855656879341659>`
-        })
+        // check if user is the host or has manage permissions
+        const isHost =
+          giveaway.hostedBy.includes(i.user.id) ||
+          giveaway.hostedBy === i.user.toString();
+        if (
+          !isHost &&
+          !i.member.permissions.has(PermissionsBitField.Flags.ManageMessages)
+        ) {
+          return i.reply({
+            content: lang.otherUser,
+            flags: MessageFlags.Ephemeral,
+          });
+        }
 
-        const resumegiveaway = new ActionRowBuilder()
-            .addComponents(
-                new StringSelectMenuBuilder()
-                    .setCustomId("resume-giveaway")
-                    .setPlaceholder(lang.selectmenu.choose)
-                    .addOptions(options),
-            )
+        const messageID = i.values[0];
 
-        await interaction.reply({ content: lang.resume.forresume, components: [resumegiveaway], flags: MessageFlags.Ephemeral })
+        client.manager
+          .unpause(messageID)
+          .then(() => {
+            i.update({ content: lang.resume.done(messageID), components: [] });
+          })
+          .catch((err) => {
+            client.log(`Resume Error: ${err.message}`, "error");
+            i.update({ content: lang.resume.errmod, components: [] });
+          });
+      }
+    });
 
-        const filter = (i) => interaction.user.id === i.user.id
-
-        const collector = interaction.channel.createMessageComponentCollector({ filter, max: 1, time: 300000 });
-
-        collector.on('collect', (interaction) => {
-            if (interaction.values[0] === "cancel") {
-                interaction.update({ content: lang.cancel.cancelled, components: [] })
-            } else {
-
-                const messageID = interaction.values[0];
-
-                client.manager.unpause(interaction.values[0])
-                    .then(() => {
-                        interaction.reply(lang.resume.done(messageID));
-                    })
-                    .catch((err) => {
-                        interaction.reply(lang.resume.errmod);
-                    });
-            }
-        })
-
-        collector.on('end', (collected, reason) => {
-            if (reason == "time") {
-                interaction.editReply({
-                    content: lang.collector.time,
-                    components: [],
-                });
-            }
-        });
-    }
+    collector.on("end", (collected, reason) => {
+      if (reason == "time") {
+        interaction
+          .editReply({
+            content: lang.collector.time,
+            components: [],
+          })
+          .catch(() => null);
+      }
+    });
+  },
 };
